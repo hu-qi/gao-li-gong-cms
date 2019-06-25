f0bcc59942d1e6f6fe887f8d0272d947704ac96f
徐家琪 <kkxujq@gmail.com>
2019-06-25 23:51:17 +0800

    feat: img upload

---
+++ src/components/ImgUpload/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Upload, Icon, Modal } from 'antd';
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
+
+class PicturesWall extends React.Component {
+  state = {
+    previewVisible: false,
+    previewImage: '',
+  };
+
+  handleCancel = () => this.setState({ previewVisible: false });
+
+  handlePreview = async file => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+
+    this.setState({
+      previewImage: file.url || file.preview,
+      previewVisible: true,
+    });
+  };
+
+  handleChange = ({ fileList }) => {
+    const {
+      onChange,
+      type,
+    } = this.props;
+
+    onChange(fileList, type);
+  };
+
+  render() {
+    const {
+      previewVisible,
+      previewImage,
+    } = this.state;
+    const {
+      limit,
+      fileList,
+    } = this.props;
+    const uploadUrl = 'api/upload/image';
+    const uploadButton = (
+      <div>
+        <Icon type='plus' />
+        <div className='ant-upload-text'>Upload</div>
+      </div>
+    );
+
+    return (
+      <div className='clearfix'>
+        <Upload
+          listType='picture-card'
+          action={uploadUrl}
+          fileList={fileList}
+          onPreview={this.handlePreview}
+          onChange={this.handleChange}
+        >
+          {fileList.length >= +limit ? null : uploadButton}
+        </Upload>
+        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+          <img alt='example' style={{ width: '100%' }} src={previewImage} />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default PicturesWall;
