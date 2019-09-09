f78f57896e14d994f279486ae91c37f745d18307
徐家琪 <xujiaqi.0@bytedance.com>
2019-09-09 15:46:09 +0800

    feat: 增加全局错误提示

--- src/services/api.js
+++ src/services/api.js
@@ -1,111 +1,30 @@
 import { stringify } from 'qs';
-import { extend } from 'umi-request';
-import request from '@/utils/request';
+import Request from '@/utils/request';
+import { notification } from 'antd';
 
-extend({
-  mode: 'no-cors',
-});
+async function request(url, opts) {
+  const response = await Request(url, opts);
+  const {
+    error,
+    isError,
+  } = response;
+  const {
+    code = '',
+    message = '',
+  } = error || {};
+
+  if (isError) {
+    notification.error({
+      message: `请求错误: ${url}`,
+      description: `${code} ${message}`,
+    });
+  }
+
+  return response;
+}
 
 export async function queryProjectNotice() {
   return request('/api/project/notice');
-}
-
-export async function queryActivities() {
-  return request('/api/activities');
-}
-
-export async function queryRule(params) {
-  return request(`/api/rule?${stringify(params)}`);
-}
-
-export async function removeRule(params) {
-  return request('/api/rule', {
-    method: 'POST',
-    data: {
-      ...params,
-      method: 'delete',
-    },
-  });
-}
-
-export async function addRule(params) {
-  return request('/api/rule', {
-    method: 'POST',
-    data: {
-      ...params,
-      method: 'post',
-    },
-  });
-}
-
-export async function updateRule(params = {}) {
-  return request(`/api/rule?${stringify(params.query)}`, {
-    method: 'POST',
-    data: {
-      ...params.body,
-      method: 'update',
-    },
-  });
-}
-
-export async function fakeSubmitForm(params) {
-  return request('/api/forms', {
-    method: 'POST',
-    data: params,
-  });
-}
-
-export async function fakeChartData() {
-  return request('/api/fake_chart_data');
-}
-
-export async function queryTags() {
-  return request('/api/tags');
-}
-
-export async function queryBasicProfile(id) {
-  return request(`/api/profile/basic?id=${id}`);
-}
-
-export async function queryAdvancedProfile() {
-  return request('/api/profile/advanced');
-}
-
-export async function queryFakeList(params) {
-  return request(`/api/fake_list?${stringify(params)}`);
-}
-
-export async function removeFakeList(params) {
-  const { count = 5, ...restParams } = params;
-  return request(`/api/fake_list?count=${count}`, {
-    method: 'POST',
-    data: {
-      ...restParams,
-      method: 'delete',
-    },
-  });
-}
-
-export async function addFakeList(params) {
-  const { count = 5, ...restParams } = params;
-  return request(`/api/fake_list?count=${count}`, {
-    method: 'POST',
-    data: {
-      ...restParams,
-      method: 'post',
-    },
-  });
-}
-
-export async function updateFakeList(params) {
-  const { count = 5, ...restParams } = params;
-  return request(`/api/fake_list?count=${count}`, {
-    method: 'POST',
-    data: {
-      ...restParams,
-      method: 'update',
-    },
-  });
 }
 
 export async function queryNotices(params = {}) {
--- src/utils/request.js
+++ src/utils/request.js
@@ -28,6 +28,7 @@ const codeMessage = {
  * 异常处理程序
  */
 const errorHandler = error => {
+  console.log('errorHandler: ', error);
   const { response = {} } = error;
   const errortext = codeMessage[response.status] || response.statusText;
   const { status, url } = response;
@@ -67,6 +68,7 @@ const errorHandler = error => {
 const request = extend({
   errorHandler, // 默认错误处理
   credentials: 'include', // 默认请求是否带上cookie
+  // mode: 'no-cors',
 });
 
 export default request;
